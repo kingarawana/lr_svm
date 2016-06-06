c68d2a34f4cf558e74663a19ecad3b2ce4e78650
Khon Lieu <klieu@edmunds.com>
2016-06-06 07:16:11 -0700

    minor updates

--- files/linear_svm.py
+++ files/linear_svm.py
@@ -1,8 +1,14 @@
 import numpy as np
 
-def svm_loss_naive(W, X, y, reg):
+def svm_loss_naive(W, X, y, reg, delta=1):
   loss = 0.0
   dW = np.zeros(W.shape)
+
+  '''
+  W = 10x3073
+  X = 3073x300
+  y = 300
+  '''
 
   #####################################
   # Your Code Here
@@ -15,7 +21,7 @@ def svm_loss_naive(W, X, y, reg):
     correct_score = scores[y[i]]
     for j in range(num_classes):
       if(j != y[i]):
-        margin = scores[j] - correct_score + 1
+        margin = scores[j] - correct_score + delta
         if(margin > 0):
           loss += margin
 
@@ -37,9 +43,15 @@ def svm_loss_naive(W, X, y, reg):
 
   return loss, dW;
 
-def svm_loss_vectorized(W, X, y, reg):
+def svm_loss_vectorized(W, X, y, reg, delta=1):
   loss = 0.0
   dW = np.zeros(W.shape)
+
+  '''
+  W = 10x3073
+  X = 3073x300
+  y = 300
+  '''
 
   #####################################
   # Your Code Here
@@ -47,10 +59,11 @@ def svm_loss_vectorized(W, X, y, reg):
   num_classes = W.shape[0]
   num_images = X.shape[1]
 
-  scores = W.dot(X)
-  correct_scores = scores[y, range(num_images)]
-  margins = scores - correct_scores + 1
-  margins[y, range(num_images)] = 0
+  scores = W.dot(X) # 10x300
+  correct_scores = scores[y, range(num_images)] # the correct scores for each of the 300 images
+  margins = scores - correct_scores + delta
+
+  margins[y, range(num_images)] = 0 # we have to zero out correct values b/c of + delta
   margins = np.maximum(np.zeros((num_classes, num_images)), margins)
 
   loss = np.sum(margins)
