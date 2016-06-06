25567f3f103787a378c647fd650bd6108e74c844
Khon Lieu <klieu@edmunds.com>
2016-06-06 07:16:32 -0700

    added docs

--- files/linear_svm.py
+++ files/linear_svm.py
@@ -4,6 +4,12 @@ def svm_loss_naive(W, X, y, reg, delta=1):
   loss = 0.0
   dW = np.zeros(W.shape)
 
+  '''
+  W = 10x3073
+  X = 3073x300
+  y = 300
+  '''
+
   #####################################
   # Your Code Here
 
@@ -19,6 +25,12 @@ def svm_loss_naive(W, X, y, reg, delta=1):
 def svm_loss_vectorized(W, X, y, reg, delta=1):
   loss = 0.0
   dW = np.zeros(W.shape)
+
+  '''
+  W = 10x3073
+  X = 3073x300
+  y = 300
+  '''
 
   #####################################
   # Your Code Here
