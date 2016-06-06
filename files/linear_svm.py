dd795c9d74ee12de1a9b4eb22379a06f8e2712f6
Khon Lieu <klieu@edmunds.com>
2016-06-06 06:18:54 -0700

    added arg

--- files/linear_svm.py
+++ files/linear_svm.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def svm_loss_naive(W, X, y, reg):
+def svm_loss_naive(W, X, y, reg, delta=1):
   loss = 0.0
   dW = np.zeros(W.shape)
 
@@ -16,7 +16,7 @@ def svm_loss_naive(W, X, y, reg):
 
   return loss, dW;
 
-def svm_loss_vectorized(W, X, y, reg):
+def svm_loss_vectorized(W, X, y, reg, delta=1):
   loss = 0.0
   dW = np.zeros(W.shape)
 
