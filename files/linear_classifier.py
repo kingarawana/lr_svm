c73ceb2354647daf21ebb36c6b021d2e392618af
Khon Lieu <klieu@edmunds.com>
2016-06-13 07:16:25 -0700

    today's session

---
+++ files/linear_classifier.py
@@ -0,0 +1,75 @@
+import numpy as np
+from files.linear_svm import *
+
+class LinearSVM:
+  def __init__(self):
+    self.W = None
+
+  # returns (loss, grad)
+  def loss(self, X_batch, y_batch, reg):
+    loss = None
+    grad = None
+    #####################################
+    # Your Code Here
+    # Hint: reuse the functions you've already implemented in linear_svm
+
+    
+    
+    #####################################
+    return loss, grad
+
+
+  # Returns an array of loss values per iteration
+  def train(self, X, y, learning_rate=1e-3, reg=1e-5, num_iters=100, 
+    batch_size=200):
+
+    dim, num_train = X.shape
+    num_classes = np.max(y) + 1 # assume y takes values 0...K-1 where K is number of classes
+    
+    if self.W is None:
+      self.W = np.random.randn(num_classes, dim) * 0.001
+
+    loss_history = []
+    for it in range(num_iters):
+      X_batch = None
+      y_batch = None
+
+      indicies = np.random.choice(num_train, batch_size, replace=True)
+      X_batch = X[:, indicies]
+      y_batch = y[indicies]
+      
+      #####################################
+      # Your Code Here
+
+      # Hint: calculate loss e.g. loss, grad = ....
+      # then add loss to history
+      
+      
+
+      #####################################
+
+      if it % 100 == 0:
+        print 'iteration %d / %d: loss %f' % (it, num_iters, loss)
+
+      #####################################
+      # Your Code Here
+
+      # Hint: Update self.W by calculated gradients. Remember to apply learning rate
+      
+
+
+      #####################################
+
+    return loss_history
+
+  def predict(self, X):
+    y_pred = np.zeros(X.shape[1])
+
+    #####################################
+    # Your Code Here
+
+    
+
+    #####################################
+
+    return y_pred
--- files/linear_svm.py
+++ files/linear_svm.py
@@ -14,7 +14,29 @@ def svm_loss_naive(W, X, y, reg, delta=1):
   # Your Code Here
 
 
+  num_classes = W.shape[0]
+  num_images = len(y)
 
+  for i in range(num_images):
+    scores = W.dot(X[:, i]) # each row is a class score
+    correct_score = scores[y[i]]
+    for j in range(num_classes):
+      if(j != y[i]):
+        margin = scores[j] - correct_score + delta
+        if(margin > 0):
+          loss += margin
+
+          dW[j] += X[:, i]
+          dW[y[i]] -= X[:, i]
+
+
+  
+  dW += reg * W
+
+  loss /= num_images
+  dW /= num_images
+
+  loss += 0.5 * reg * np.sum(W * W)
 
   
 
@@ -36,10 +58,6 @@ def svm_loss_vectorized(W, X, y, reg, delta=1):
   # Your Code Here
 
 
-
-
-
-  
 
   #####################################
 
